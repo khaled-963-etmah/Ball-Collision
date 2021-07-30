88413f03efbb3f0dc34b3c63de933ab67cb7507c
khaled-963-etmah <72010411+khaled-963-etmah@users.noreply.github.com>
2021-07-30 19:57:03 +0300

    Add files via upload

---
+++ dist/inviroment/render.js
@@ -0,0 +1,18 @@
+import * as THREE from '../../node_modules/three/src/Three.js';
+
+class Render {
+
+    constructor() {
+        this.renderer = new THREE.WebGLRenderer({
+            antialias: false
+        });
+        this.renderer.shadowMap.enabled = true;
+        this.renderer.shadowMap.type    = THREE.PCFSoftShadowMap;
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        document.body.appendChild(this.renderer.domElement);
+        return this.renderer;
+    }
+}
+
+export default Render;
